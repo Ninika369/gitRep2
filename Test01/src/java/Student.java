93edf1d6fc50f0113a9395c35a86f53e2bbe142b
George <975943143@qq.com>
2022-11-24 14:01:17 +0800

    第一次使用idea提交git

---
+++ Test01/src/java/Student.java
@@ -0,0 +1,16 @@
+package java;
+
+/**
+ * @Author: George Sun
+ * @Date: 2022-11-24-13:54
+ * @Description: java
+ */
+public class Student {
+    public int weight;
+    public int age;
+    public String gender;
+
+    public Student() {
+
+    }
+}
