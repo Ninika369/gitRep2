bc5cf82c2a11d57ab31bbf3443729c49346c0caf
George <975943143@qq.com>
2022-11-24 15:16:06 +0800

    修改Person类

--- Test01/src/java/Person.java
+++ Test01/src/java/Person.java
@@ -2,4 +2,12 @@ package java;
 
 public class Person {
     int height;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
